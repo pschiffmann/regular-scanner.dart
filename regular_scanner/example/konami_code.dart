ca528b1848a4b24738354fe3f56b5064f9fbf3b4
Philipp Schiffmann <philippschiffmann93@gmail.com>
2019-07-03 22:58:02 +0200

    Use Unicode escape sequences in konami code example

--- regular_scanner/example/konami_code.dart
+++ regular_scanner/example/konami_code.dart
@@ -1,20 +1,25 @@
+// To try out this example:
+//  1. `pub global activate webdev`
+//  2. `pub global run webdev serve example/`
+//  3. Visit http://localhost/konami_code.html
+
 import 'dart:html';
 
 import 'package:regular_scanner/regular_scanner.dart';
 
 // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
-const up = '\x26';
-const down = '\x28';
-const left = '\x25';
-const right = '\x27';
-const a = '\x41';
-const b = '\x42';
+const up = r'\u{0026}';
+const down = r'\u{0028}';
+const left = r'\u{0025}';
+const right = r'\u{0027}';
+const a = r'\u{0041}';
+const b = r'\u{0042}';
 
 /// https://en.wikipedia.org/wiki/Konami_Code: `↑ ↑ ↓ ↓ ← → ← → B A`
 ///
 /// Prefixed with `.*` to skip over other key events.
 final konamiCode = Scanner.unambiguous(
-    [Regex('.*$up$up\\$down\\$down$left$right$left$right$b$a')]);
+    [Regex('.*$up$up$down$down$left$right$left$right$b$a')]);
 
 void main() {
   final stateMachine = konamiCode.stateMachine();
